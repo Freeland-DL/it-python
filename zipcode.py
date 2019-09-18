d1a0bcb5fb9278a975fb8dd894690da41776fe2c
Freeland <freeland.collin2002@gmail.com>
2019-09-18 13:19:54 -0400

    Adds zipcode.py

--- zipcode.py
+++ zipcode.py
@@ -1,3 +1,50 @@
 from banner import banner
 
 banner ("ZIP CODE SORTER", "Collin Freeland")
+
+#1. Get zipcode from the user
+#2. Read the zipcode, and pin it to the corresponding city
+#3. Print the zipcode and what city it belongs to
+#4. Ask the user if they want to enter another zip code
+#5. If yes, return to 1. If no, print goodbye message.
+
+print("Welcome to the Newaygo County zip code sorter.")
+print("")
+
+response = 'Y'
+
+while response == "Y":
+
+    zipcode = input("Please enter a zip code: ")
+    print("")
+    if zipcode == "49412":
+        print(f"The zipcode {zipcode} is for Fremont")
+        print("")
+    elif zipcode == "49309":
+        print(f"The zipcode {zipcode} is for Bitely")
+        print("")
+    elif zipcode == "49312":
+        print(f"The zipcode {zipcode} is for Brohman")
+        print("")
+    elif zipcode == "49337":
+        print(f"The zipcode {zipcode} is for Croton and Newaygo")
+        print("")
+    elif zipcode == "49413":
+        print(f"The zipcode {zipcode} is for Fremont")
+        print("")
+    elif zipcode == "49327":
+        print(f"The zipcode {zipcode} is for Grant")
+        print("")
+    elif zipcode == "49349":
+        print(f"The zipcode {zipcode} is for White Cloud")
+        print("")
+    else:
+        print(f"The zipcode {zipcode} is not in Newaygo County.")
+        print("")
+
+    response = input("Would you like to try again? [Y/N] ")
+    print("")
+    if response == "Y":
+        pass
+    elif response == "N":
+        print("Thank you for using the Newaygo County zip code sorter. Goodbye!")
